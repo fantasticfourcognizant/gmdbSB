7f9eedba34c9ae16f686d7cd3941090790b9abbf
pareshsolanki29 <bandralites@gmail.com>
2019-05-13 13:32:44 -0400

    initial push

---
+++ src/main/java/com/cognizant/gmdb_be/GmdbBeApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.gmdb_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmdbBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmdbBeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cognizant/gmdb_be/Models/Login.java
@@ -0,0 +1,10 @@
+package com.cognizant.gmdb_be.Models;
+
+
+import lombok.Data;
+
+@Data
+public class Login {
+    private String email;
+    private String password;
+}
